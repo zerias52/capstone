95b7041a40dd00d8ca074d3d50ef4965e3d01b76
zerias52 <bbyrd52@gmail.com>
2025-01-18 13:47:40 -0600

    Updated home and redirects. Tweaked CSS

--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import GeneratedPost
 from .forms import GeneratePostForm
 from .services import generate_social_post
@@ -38,7 +38,10 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         # Set the generated content
         form.instance.content = generated_content
 
-        return super().form_valid(form)
+        # Save the post
+        self.object = form.save()
+
+        return redirect('generator:post-detail', pk=self.object.pk)
 
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = GeneratedPost
